ca2cf0bf0b0637c718234de3f009ddde7deea14e
M05 <pre.mos_os@hotmail.com>
2016-07-26 16:11:21 +0700

    new update

--- api/get.php
+++ api/get.php
@@ -9,31 +9,31 @@ if (isset($_GET['var1']) || isset($_GET['var2']) || isset($_GET['var3'])){
 
   include("dbconnect.php");
 
-  if (isset($_GET['var1']) && !isset($_GET['var2']) && !isset($_GET['var3'])){
+  if (isset($_GET['var1'])){
     if (ctype_digit($_GET['var1'])){
-      $sql = "SELECT AMPHUR_ID, DISTRICT_ID, DISTRICT_NAME FROM districts ORDER BY DISTRICT_NAME ASC";
+      $sql = "SELECT PROVINCE_ID, PROVINCE_NAME FROM provinces WHERE  GEO_ID = ".$_GET['var1']." ORDER BY PROVINCE_NAME ASC";
       $result = $mysqli->query($sql);
-      $a = array();
-      while($row = $result->fetch_assoc()){
-        array_push($a,array('AMPHUR_ID' => $row['AMPHUR_ID'],'DISTRICT_ID' => $row['DISTRICT_ID'],  'DISTRICT_NAME' => $row['DISTRICT_NAME']));
-      }
-      echo json_encode($a);
+      echo json_encode($result->fetch_all());
     }else{
       exit();
     }
   }
 
-  if (isset($_GET['var1']) && isset($_GET['var2']) && !isset($_GET['var3'])){
-    if (ctype_digit($_GET['var1']) && ctype_digit($_GET['var2'])){
-      echo 'xxx2';
+  if (isset($_GET['var2'])){
+    if (ctype_digit($_GET['var2'])){
+      $sql = "SELECT AMPHUR_ID, AMPHUR_NAME FROM amphures WHERE  PROVINCE_ID = ".$_GET['var2']." ORDER BY AMPHUR_NAME ASC";
+      $result = $mysqli->query($sql);
+      echo json_encode($result->fetch_all());
     }else{
       exit();
     }
   }
 
-  if (isset($_GET['var1']) && isset($_GET['var2']) && isset($_GET['var3'])){
-    if (ctype_digit($_GET['var1']) && ctype_digit($_GET['var2']) && ctype_digit($_GET['var3'])){
-      echo 'xxx3';
+  if (isset($_GET['var3'])){
+    if (ctype_digit($_GET['var3'])){
+      $sql = "SELECT DISTRICT_ID, DISTRICT_NAME FROM districts WHERE  AMPHUR_ID = ".$_GET['var3']." ORDER BY DISTRICT_NAME ASC";
+      $result = $mysqli->query($sql);
+      echo json_encode($result->fetch_all());
     }else{
       exit();
     }
